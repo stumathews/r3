207e7637ebf16be96fc24cc539b4c49eef6c58c4
Stuart Mathews <stumathews@gmail.com>
2015-08-18 18:46:29 +0100

    cleanup

--- prs/src/main/java/configuration/SecurityConfig.java
+++ prs/src/main/java/configuration/SecurityConfig.java
@@ -9,11 +9,8 @@ package configuration;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.datasource.AbstractDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
