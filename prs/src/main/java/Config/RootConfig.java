4ee08e99d6180d33a2f79022690e69742b76a217
Stuart Mathews <stumathews@gmail.com>
2015-08-28 11:20:24 +0100

    remove some entity files and move middle tier beans to ContextLoader content for creation there

--- prs/src/main/java/Config/RootConfig.java
+++ prs/src/main/java/Config/RootConfig.java
@@ -6,10 +6,20 @@
 
 package Config;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -23,5 +33,95 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
             @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
         })
 public class RootConfig {
+  @Bean
+  public BOL.Interfaces.IUser userBOL()
+  {
+    return new BOL.User();
+  }
+    
+  @Bean
+  public DAL.Interfaces.IUserDAO userDAO()
+  {
+    return new DAL.UserDAO();
+  }
   
+  
+  
+  @Bean
+  public BOLO.R3GlobalConfig r3config()
+  {
+    BOLO.R3GlobalConfig global = new BOLO.R3GlobalConfig();
+    
+    return global;    
+  }
+  
+   @Bean   
+   public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+      HibernateTransactionManager txManager = new HibernateTransactionManager();
+      txManager.setSessionFactory(sessionFactory);
+ 
+      return txManager;
+   }
+  
+   Properties hibernateProperties() {
+      return new Properties() {
+         {
+            setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+            setProperty("hibernate.show_sql", "true");
+            setProperty("hibernate.c3p0.min_size", "5");
+            setProperty("hibernate.c3p0.max_size", "20");
+            setProperty("hibernate.c3p0.timeout", "300");
+            setProperty("hibernate.c3p0.max_statements", "50");
+            setProperty("hibernate.c3p0.idle_test_period", "3000");
+            setProperty("hibernate.hbm2ddl.auto", "create");
+         }
+      };
+   }
+  
+   
+  @Bean 
+  public org.springframework.jdbc.datasource.DriverManagerDataSource myDataSource()
+  {
+    DriverManagerDataSource myDataSource = new DriverManagerDataSource();
+    myDataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+    myDataSource.setUrl("jdbc:hsqldb:mem:r3");
+    myDataSource.setUsername("sa");
+    myDataSource.setPassword("");
+    return myDataSource;    
+  }
+  
+    
+  @Bean
+  public LocalSessionFactoryBean sessionFactory(DataSource myDataSource)
+  {    
+    
+   LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+   sessionFactory.setDataSource(myDataSource);
+   Properties properties = new Properties();    
+   
+   properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+   properties.put("hibernate.show_sql", "true");
+   properties.put("hibernate.c3p0.min_size", "5");
+   properties.put("hibernate.c3p0.max_size", "20");
+   properties.put("hibernate.c3p0.timeout", "300");
+   properties.put("hibernate.c3p0.max_statements", "50");
+   properties.put("hibernate.c3p0.idle_test_period", "3000");
+   properties.put("hibernate.hbm2ddl.auto", "create");
+   sessionFactory.setMappingResources( new String[] {
+     "DEL/User.hbm.xml"
+   });
+   sessionFactory.setHibernateProperties(properties);   
+   
+   return sessionFactory;
+  }
+    
+  @Bean public HibernateExceptionTranslator hibernateExceptionTranslator()
+  {
+    return new HibernateExceptionTranslator();
+  }
+  
+  @Bean 
+  public BeanPostProcessor persistenceTranslation(){    
+    return new PersistenceExceptionTranslationPostProcessor();
+  }
 }
--- prs/src/main/java/Config/WebConfig.java
+++ prs/src/main/java/Config/WebConfig.java
@@ -82,102 +82,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     templateResolver.setSuffix(".jsp");
     templateResolver.setTemplateMode("HTML5");
     return templateResolver;    
-  }
-  
- 
-  @Bean
-  public BOL.Interfaces.IUser userBOL()
-  {
-    return new BOL.User();
-  }
-    
-  @Bean
-  public DAL.Interfaces.IUserDAO userDAO()
-  {
-    return new DAL.UserDAO();
-  }
-  
-  
-  
-  @Bean
-  public BOLO.R3GlobalConfig r3config()
-  {
-    BOLO.R3GlobalConfig global = new BOLO.R3GlobalConfig();
-    
-    return global;    
-  }
-  
-   @Bean   
-   public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-      HibernateTransactionManager txManager = new HibernateTransactionManager();
-      txManager.setSessionFactory(sessionFactory);
- 
-      return txManager;
-   }
-  
-   Properties hibernateProperties() {
-      return new Properties() {
-         {
-            setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-            setProperty("hibernate.show_sql", "true");
-            setProperty("hibernate.c3p0.min_size", "5");
-            setProperty("hibernate.c3p0.max_size", "20");
-            setProperty("hibernate.c3p0.timeout", "300");
-            setProperty("hibernate.c3p0.max_statements", "50");
-            setProperty("hibernate.c3p0.idle_test_period", "3000");
-            setProperty("hibernate.hbm2ddl.auto", "create");
-         }
-      };
-   }
-  
-   
-  @Bean 
-  public org.springframework.jdbc.datasource.DriverManagerDataSource myDataSource()
-  {
-    DriverManagerDataSource myDataSource = new DriverManagerDataSource();
-    myDataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-    myDataSource.setUrl("jdbc:hsqldb:mem:r3");
-    myDataSource.setUsername("sa");
-    myDataSource.setPassword("");
-    return myDataSource;    
-  }
-  
-    
-  @Bean
-  public LocalSessionFactoryBean sessionFactory(DataSource myDataSource)
-  {    
-    
-   LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-   sessionFactory.setDataSource(myDataSource);
-   Properties properties = new Properties();    
-   
-   properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-   properties.put("hibernate.show_sql", "true");
-   properties.put("hibernate.c3p0.min_size", "5");
-   properties.put("hibernate.c3p0.max_size", "20");
-   properties.put("hibernate.c3p0.timeout", "300");
-   properties.put("hibernate.c3p0.max_statements", "50");
-   properties.put("hibernate.c3p0.idle_test_period", "3000");
-   properties.put("hibernate.hbm2ddl.auto", "create");
-   sessionFactory.setMappingResources( new String[] {
-     "DEL/Token.hbm.xml",
-     "DEL/User.hbm.xml"
-   });
-   sessionFactory.setHibernateProperties(properties);   
-   
-   return sessionFactory;
-  }
-    
-  @Bean public HibernateExceptionTranslator hibernateExceptionTranslator()
-  {
-    return new HibernateExceptionTranslator();
-  }
-  
-  @Bean 
-  public BeanPostProcessor persistenceTranslation(){    
-    return new PersistenceExceptionTranslationPostProcessor();
-  }
-  
+  }  
   @Bean
   public ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver()
   {
@@ -214,11 +119,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
     resourceBundleMessageSource.setBasename("Messages");
     return resourceBundleMessageSource;
-  }
-  
-  @Bean
-  public SimpleJaxWsServiceExporter jaxWsServiceExporter(){
-    return new SimpleJaxWsServiceExporter();
-  }
-    
+  }    
 }
