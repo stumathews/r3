fd2ef218ae676bc6f757e1210156c114edbf874d
Stuart Mathews <stumathews@gmail.com>
2015-08-28 11:02:42 +0100

    removed some more

--- prs/src/main/java/DAL/Interfaces/ICharacteristicReviewDAO.java
+++
@@ -1,6 +0,0 @@
-package DAL.Interfaces;
-
-public interface ICharacteristicReviewDAO 
-{
-    DEL.CharacteristicReview getCharacteristicReview(Long id) throws Exception;
-}
--- prs/src/main/java/DEL/User.java
+++ prs/src/main/java/DEL/User.java
@@ -12,73 +12,47 @@ public class User implements Serializable
     private Long id;
     private String username;
     private String password;
-    private Set<Token> tokens;
-    private Set<Review> reviews;
-    private Set<DEL.Recommendation> recommendations;
+   
+    public User(){}
+    public User( String username, String password){
+            setUsername(username);
+            setPassword(password);
+    }
+    /**
+     * @return the id
+     */
 
-    public Set<Recommendation> getRecommendations() {
-        return recommendations;
+    public Long getId() {
+            return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+            this.id = id;
+    }
+    /**
+     * @return the username
+     */
+
+    public String getUsername() {
+            return username;
+    }
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+            this.username = username;
     }
 
-    public void setRecommendations(Set<Recommendation> recommendations) {
-        this.recommendations = recommendations;
+
+    public String getPassword() { 
+            return password;
     }
 
-    public Set<Review> getReviews() {
-        return reviews;
+    public void setPassword(String password) {
+            this.password = password;
     }
-
-    public void setReviews(Set<Review> reviews) {
-        this.reviews = reviews;
-    }
-
-    public Set<Token> getTokens() {
-        return tokens;
-    }
-
-    public void setTokens(Set<Token> tokens) {
-        this.tokens = tokens;
-    }
-	public User(){}
-	public User( String username, String password){
-		setUsername(username);
-		setPassword(password);
-	}
-	/**
-	 * @return the id
-	 */
-	
-	public Long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-	/**
-	 * @return the username
-	 */
-	
-	public String getUsername() {
-		return username;
-	}
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	
-	public String getPassword() { 
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
     
     @Override
     public int hashCode(){      
