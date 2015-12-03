9840f7da5f094425afc7fc095128fc5f1541c0ac
Stuart Mathews <stumathews@gmail.com>
2015-12-03 10:35:05 +0000

    add general erro handler with exception message in page shown

--- src/main/java/Website/Controllers/ErrorHandling/GlobalErrorHandler.java
+++ src/main/java/Website/Controllers/ErrorHandling/GlobalErrorHandler.java
@@ -1,38 +1,8 @@
-/*
- * Copyright (c) 2015, Stuart Mathews
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 package Website.Controllers.ErrorHandling;
-
-import java.io.IOException;
-import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles the exceptions that aren't handled by the controllers themselves
@@ -49,8 +19,12 @@ public class GlobalErrorHandler
     }
     
     @ExceptionHandler(Exception.class)
-    public String handleGeneralException(HttpServletRequest request, Exception ex)
-    {
-        return "Errors/database_error";
+    public ModelAndView  handleGeneralException(HttpServletRequest request, Exception ex)
+    {   
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName("Errors/general_error");
+    return mav;
     }
 }
